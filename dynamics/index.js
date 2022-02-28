bdc3a294d973f6d24a6e1c61e0ca4d7854397f81
Francisco-Galindo <paqui10718@gmail.com>
2022-02-28 09:16:47 -0600

    Canvas reactivo

--- dynamics/index.js
+++ dynamics/index.js
@@ -2,36 +2,55 @@ function acciones (evento) {
 
     const opt = evento.target.innerText;
 
-    switch(opt) {
+    switch (opt) {
         case 'Jugar':
-            console.log("Vamo a jugar");
+            window.location = "./juego.html";
             break;
-            
+
         case 'Instrucciones':
             console.log("Vamo a leer instrucciones");
             break;
-        
+
         case 'Créditos':
             console.log("Vamo a leer créditos");
             break;
     }
 }
 
-window.onload = () =>{
-    
-    const opciones = document.getElementById('opciones');
+window.onload = () => {
 
-    //Seleccionar con el mouse
-    opciones.addEventListener('click', evento => acciones(evento));
+    let indice = 0;
+    const opcionesDiv = document.getElementById('opciones');
+    const opciones = Array.from(document.getElementsByClassName('opcion'));
+    opciones[indice].focus();
 
-    //Seleccionar mediante teclado (tab y enter/espacio)
-    opciones.addEventListener('keydown', evento => {
-        if (evento.code === "Enter" | evento.code === "Space") {
-            acciones(evento); 
+    // Seleccionar con el mouse
+    opcionesDiv.addEventListener('mouseover', (evento) => {
+        indiceTarget = opciones.indexOf(evento.target);
+        indice = (indiceTarget >= 0) ? indiceTarget : indice;
+        evento.target.focus();
+    });
+    opcionesDiv.addEventListener('click', evento => acciones(evento));
+
+    // Seleccionar mediante teclado (tab y enter/espacio)
+    opcionesDiv.addEventListener('keydown', evento => {
+        if (evento.code === "Enter" || evento.code === "Space") {
+            acciones(evento);
         }
     });
 
+    // Elegir opcion con las flechas
+    window.addEventListener('keydown', evento => {
+        switch (evento.key) {
+            case 'ArrowUp':
+                indice--;
+                indice = (indice >= 0) ? indice : opciones.length - 1;
+                opciones[indice].focus();
+                break;
+            case 'ArrowDown':
+                indice = (indice + 1) % opciones.length;
+                opciones[indice].focus();
+                break
+        }
+    })
 };
-
-
-
---
+++ dynamics/juego.js
@@ -0,0 +1,66 @@
+let juegoDiv;
+let juegoCanvas;
+let ctx;
+let drawnFrames = 0;
+let inicio;
+let fuente = new FontFace('fuente', 'url(./statics/fonts/I-pixel-u.ttf)')
+let factor = 16;
+let factorJuan = 0;
+let virtualHeight = 480;
+let virtualWidth = 672;
+
+function cicloJuego() {
+    ctx.beginPath();
+    ctx.fillStyle = "#0096A7";
+    ctx.fillRect(0, 0, juegoCanvas.width, juegoCanvas.height);
+    ctx.closePath();
+
+    dibujarPantallaJugadores(ctx, factor);
+    mostrarFramerate();
+    requestAnimationFrame(cicloJuego);
+}
+
+function mostrarFramerate() {
+    ctx.beginPath();
+    ctx.textAlign = "center";
+    ctx.fillStyle = "#FFFFFF";
+    ctx.font = "20px monospace";
+    let framerate = Math.floor(drawnFrames / ((Date.now() - inicio) / 1000));
+    ctx.fillText(`${framerate} fps`, 50, 50);
+    ctx.closePath();
+    drawnFrames++;
+}
+
+function redimensionarCanvas() {
+    juegoCanvas.height = juegoDiv.clientHeight;
+    juegoCanvas.width = juegoDiv.clientWidth;
+    factor = 32 * (juegoCanvas.width / virtualWidth);
+    factorJuan = juegoCanvas.width / virtualWidth;
+    console.log(factor);
+}
+
+function posicionMouse(evento) {
+    let clientRect = juegoCanvas.getBoundingClientRect();
+    return {
+        x: (evento.clientX - clientRect.left) / factorJuan,
+        y: (evento.clientY - clientRect.top) / factorJuan,
+    }
+}
+
+window.addEventListener("resize", redimensionarCanvas);
+
+document.addEventListener("DOMContentLoaded", () => {
+    juegoDiv = document.getElementById("juego");
+    juegoCanvas = document.getElementById("juego-canvas");
+    ctx = juegoCanvas.getContext("2d");
+
+    ctx.imageSmoothingEnabled = false;
+    redimensionarCanvas();
+
+    juegoCanvas.addEventListener("click", (evento) => {
+        console.log(posicionMouse(evento));
+    });
+
+    inicio = new Date();
+    cicloJuego();
+});
